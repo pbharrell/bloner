ca897b09dca542859c0cb545ab38824bc640c9a9
pbharrell <backstroker2001@gmail.com>
2026-06-14 14:29:57 -0400

    Move pages...

--- page_lobby_request_input.go
+++ page_lobby_request_input.go
@@ -11,7 +11,7 @@ type LobbyRequestInputPage struct {
 	game            *Game
 	buttonBack      *Button
 	buttonJoinLobby *Button
-	lobbyRequestStr string
+	LobbyRequestStr string
 	txtInputBox     *graphics.Shape
 	txtOp           text.DrawOptions
 	fontFace        text.GoTextFace
@@ -21,40 +21,37 @@ type LobbyRequestInputPage struct {
 func CreateLobbyRequestInputPage(game *Game) *LobbyRequestInputPage {
 	lobbyRequestStr := "Id of lobby you'd like to join:"
 
-	overlay := *graphics.CreateRectangle(overlayImage, screenWidth, screenHeight, 0, 0, 0, 0, 0, 0)
+	overlay := *graphics.CreateRectangle(OverlayImage, ScreenWidth, ScreenHeight, 0, 0, 0, 0, 0, 0)
 
-	// Create font faces with different sizes as needed
 	fontFace := text.GoTextFace{
-		Source: game.fontSource,
+		Source: game.GetFontSource(),
 		Size:   32,
 	}
 
 	txtOp := text.DrawOptions{}
 
 	lobbyRequestInputTxtW, lobbyRequestInputTxtH := text.Measure(lobbyRequestStr, &fontFace, 0)
-	txtOp.GeoM.Translate(screenWidth/2-lobbyRequestInputTxtW/2, screenHeight/2-lobbyRequestInputTxtH)
+	txtOp.GeoM.Translate(ScreenWidth/2-lobbyRequestInputTxtW/2, ScreenHeight/2-lobbyRequestInputTxtH)
 
-	txtInputBox := graphics.CreateRectangle(txtInputBoxImage, int(lobbyRequestInputTxtW)+10, int(lobbyRequestInputTxtH)+10, int(screenWidth/2-lobbyRequestInputTxtW/2-5), int(screenHeight/2+lobbyRequestInputTxtH-5), 0, 0, 0, 0)
+	txtInputBox := graphics.CreateRectangle(TxtInputBoxImage, int(lobbyRequestInputTxtW)+10, int(lobbyRequestInputTxtH)+10, int(ScreenWidth/2-lobbyRequestInputTxtW/2-5), int(ScreenHeight/2+lobbyRequestInputTxtH-5), 0, 0, 0, 0)
 
 	p := &LobbyRequestInputPage{
-		game: game,
-		// buttonBack:      buttonBack,
-		// buttonJoinLobby: buttonJoinLobby,
-		lobbyRequestStr: lobbyRequestStr,
+		game:            game,
+		LobbyRequestStr: lobbyRequestStr,
 		txtInputBox:     txtInputBox,
 		txtOp:           txtOp,
 		fontFace:        fontFace,
 		overlay:         overlay,
 	}
 
-	p.buttonBack = CreateButton(p, game, newLobby, "assets/new_lobby_button.png", "assets/new_lobby_button_pressed.png", 4, 0, screenHeight/2+80, 0)
+	p.buttonBack = CreateButton(p, game, newLobby, "assets/new_lobby_button.png", "assets/new_lobby_button_pressed.png", 4, 0, ScreenHeight/2+80, 0)
 	newLobbyWidth := p.buttonBack.sprite.ImageWidth
-	newLobbyX := screenWidth/2 - newLobbyWidth/2 - 80
+	newLobbyX := ScreenWidth/2 - newLobbyWidth/2 - 80
 	p.buttonBack.SetLoc(newLobbyX, p.buttonBack.sprite.Y)
 
-	p.buttonJoinLobby = CreateButton(p, game, joinLobby, "assets/join_lobby_button.png", "assets/join_lobby_button_pressed.png", 4, 0, screenHeight/2+80, 0)
+	p.buttonJoinLobby = CreateButton(p, game, joinLobby, "assets/join_lobby_button.png", "assets/join_lobby_button_pressed.png", 4, 0, ScreenHeight/2+80, 0)
 	joinLobbyWidth := p.buttonJoinLobby.sprite.ImageWidth
-	joinLobbyX := screenWidth/2 - joinLobbyWidth/2 + 80
+	joinLobbyX := ScreenWidth/2 - joinLobbyWidth/2 + 80
 	p.buttonJoinLobby.SetLoc(joinLobbyX, p.buttonJoinLobby.sprite.Y)
 
 	return p
@@ -67,42 +64,42 @@ func (p *LobbyRequestInputPage) Update() {
 	p.buttonBack.Update(x, y, mouseButtonPressed)
 	p.buttonJoinLobby.Update(x, y, mouseButtonPressed)
 
-	if len(p.lobbyRequestStr) < 22 {
+	if len(p.LobbyRequestStr) < 22 {
 		if inpututil.IsKeyJustPressed(ebiten.Key0) {
-			p.lobbyRequestStr += "0"
+			p.LobbyRequestStr += "0"
 		}
 		if inpututil.IsKeyJustPressed(ebiten.Key1) {
-			p.lobbyRequestStr += "1"
+			p.LobbyRequestStr += "1"
 		}
 		if inpututil.IsKeyJustPressed(ebiten.Key2) {
-			p.lobbyRequestStr += "2"
+			p.LobbyRequestStr += "2"
 		}
 		if inpututil.IsKeyJustPressed(ebiten.Key3) {
-			p.lobbyRequestStr += "3"
+			p.LobbyRequestStr += "3"
 		}
 		if inpututil.IsKeyJustPressed(ebiten.Key4) {
-			p.lobbyRequestStr += "4"
+			p.LobbyRequestStr += "4"
 		}
 		if inpututil.IsKeyJustPressed(ebiten.Key5) {
-			p.lobbyRequestStr += "5"
+			p.LobbyRequestStr += "5"
 		}
 		if inpututil.IsKeyJustPressed(ebiten.Key6) {
-			p.lobbyRequestStr += "6"
+			p.LobbyRequestStr += "6"
 		}
 		if inpututil.IsKeyJustPressed(ebiten.Key7) {
-			p.lobbyRequestStr += "7"
+			p.LobbyRequestStr += "7"
 		}
 		if inpututil.IsKeyJustPressed(ebiten.Key8) {
-			p.lobbyRequestStr += "8"
+			p.LobbyRequestStr += "8"
 		}
 		if inpututil.IsKeyJustPressed(ebiten.Key9) {
-			p.lobbyRequestStr += "9"
+			p.LobbyRequestStr += "9"
 		}
 	}
 
-	if len(p.lobbyRequestStr) > 0 {
+	if len(p.LobbyRequestStr) > 0 {
 		if inpututil.IsKeyJustPressed(ebiten.KeyBackspace) {
-			p.lobbyRequestStr = p.lobbyRequestStr[:len(p.lobbyRequestStr)-1]
+			p.LobbyRequestStr = p.LobbyRequestStr[:len(p.LobbyRequestStr)-1]
 		}
 	}
 }
@@ -117,24 +114,23 @@ func (p *LobbyRequestInputPage) Draw(screen *ebiten.Image) {
 		lobbyRequstInputTxtBoxOp = text.DrawOptions{}
 	)
 
-	// Create font faces with different sizes as needed
 	fontFace := &text.GoTextFace{
-		Source: p.game.fontSource,
+		Source: p.game.GetFontSource(),
 		Size:   32,
 	}
 
 	lobbyRequestInputTxtW, lobbyRequestInputTxtH := text.Measure(lobbyRequestInputTxt, fontFace, 0)
-	lobbyRequestInputTxtOp.GeoM.Translate(screenWidth/2-lobbyRequestInputTxtW/2, screenHeight/2-lobbyRequestInputTxtH)
+	lobbyRequestInputTxtOp.GeoM.Translate(ScreenWidth/2-lobbyRequestInputTxtW/2, ScreenHeight/2-lobbyRequestInputTxtH)
 	text.Draw(screen, lobbyRequestInputTxt, fontFace, &lobbyRequestInputTxtOp)
 
 	if p.txtInputBox == nil {
-		p.txtInputBox = graphics.CreateRectangle(txtInputBoxImage, int(lobbyRequestInputTxtW)+10, int(lobbyRequestInputTxtH)+10, int(screenWidth/2-lobbyRequestInputTxtW/2-5), int(screenHeight/2+lobbyRequestInputTxtH-5), 0, 0, 0, 0)
+		p.txtInputBox = graphics.CreateRectangle(TxtInputBoxImage, int(lobbyRequestInputTxtW)+10, int(lobbyRequestInputTxtH)+10, int(ScreenWidth/2-lobbyRequestInputTxtW/2-5), int(ScreenHeight/2+lobbyRequestInputTxtH-5), 0, 0, 0, 0)
 		println(p.txtInputBox.X, p.txtInputBox.Y)
 	}
 	p.txtInputBox.Draw(screen)
 
 	lobbyRequstInputTxtBoxOp.GeoM.Translate(float64(p.txtInputBox.X+5), float64(p.txtInputBox.Y+5))
-	text.Draw(screen, p.lobbyRequestStr, fontFace, &lobbyRequstInputTxtBoxOp)
+	text.Draw(screen, p.LobbyRequestStr, fontFace, &lobbyRequstInputTxtBoxOp)
 
 	p.buttonBack.Draw(screen, op)
 	p.buttonJoinLobby.Draw(screen, op)
--- page_lobby_type_select.go
+++ page_lobby_type_select.go
@@ -1,4 +1,4 @@
-package main
+package pages
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
@@ -15,21 +15,21 @@ type LobbyTypeSelectPage struct {
 }
 
 func CreateLobbyTypeSelectPage(game *Game) *LobbyTypeSelectPage {
-	overlay := *graphics.CreateRectangle(overlayImage, screenWidth, screenHeight, 0, 0, 0, 0, 0, 0)
+	overlay := *graphics.CreateRectangle(OverlayImage, ScreenWidth, ScreenHeight, 0, 0, 0, 0, 0, 0)
 
 	p := &LobbyTypeSelectPage{
 		game:    game,
 		overlay: overlay,
 	}
 
-	p.buttonNewLobby = CreateButton(p, game, newLobby, "assets/new_lobby_button.png", "assets/new_lobby_button_pressed.png", 4, 0, screenHeight/2+80, 0)
+	p.buttonNewLobby = CreateButton(p, game, newLobby, "assets/new_lobby_button.png", "assets/new_lobby_button_pressed.png", 4, 0, ScreenHeight/2+80, 0)
 	newLobbyWidth := p.buttonNewLobby.sprite.ImageWidth
-	newLobbyX := screenWidth/2 - newLobbyWidth/2 - 80
+	newLobbyX := ScreenWidth/2 - newLobbyWidth/2 - 80
 	p.buttonNewLobby.SetLoc(newLobbyX, p.buttonNewLobby.sprite.Y)
 
-	p.buttonJoinLobby = CreateButton(p, game, joinLobby, "assets/join_lobby_button.png", "assets/join_lobby_button_pressed.png", 4, 0, screenHeight/2+80, 0)
+	p.buttonJoinLobby = CreateButton(p, game, joinLobby, "assets/join_lobby_button.png", "assets/join_lobby_button_pressed.png", 4, 0, ScreenHeight/2+80, 0)
 	joinLobbyWidth := p.buttonJoinLobby.sprite.ImageWidth
-	joinLobbyX := screenWidth/2 - joinLobbyWidth/2 + 80
+	joinLobbyX := ScreenWidth/2 - joinLobbyWidth/2 + 80
 	p.buttonJoinLobby.SetLoc(joinLobbyX, p.buttonJoinLobby.sprite.Y)
 
 	return p
@@ -52,14 +52,13 @@ func (p *LobbyTypeSelectPage) Draw(screen *ebiten.Image) {
 		op            = ebiten.DrawImageOptions{}
 	)
 
-	// Create font faces with different sizes as needed
 	fontFace := &text.GoTextFace{
-		Source: p.game.fontSource,
+		Source: p.game.GetFontSource(),
 		Size:   32,
 	}
 
 	txtW, txtH := text.Measure(lobbyWaitText, fontFace, 0)
-	txtOp.GeoM.Translate(screenWidth/2-txtW/2, screenHeight/2-txtH/2)
+	txtOp.GeoM.Translate(ScreenWidth/2-txtW/2, ScreenHeight/2-txtH/2)
 	text.Draw(screen, lobbyWaitText, fontFace, &txtOp)
 
 	p.buttonNewLobby.Draw(screen, op)
